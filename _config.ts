6208e198f3c496517cab4ee327d7c3199b26b048
Enrique MP <enriquemp.dev@gmail.com>
2023-01-09 21:08:41 +0100

    added sitemap plugin

--- _config.ts
+++ _config.ts
@@ -1,8 +1,11 @@
 import lume from "lume/mod.ts";
+import sitemap from "lume/plugins/sitemap.ts";
 
 const site = lume({
   location: new URL("https://enriquemp.ml"),
   src: "./src",
 });
+
+site.use(sitemap());
 
 export default site;
